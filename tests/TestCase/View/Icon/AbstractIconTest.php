d127413e47d42c5146569c316d90acb7e3bc2c8a
mscherer <dereuromark@web.de>
2025-10-28 17:07:42 +0100

    Fix tests

--- tests/TestCase/View/Icon/AbstractIconTest.php
+++ tests/TestCase/View/Icon/AbstractIconTest.php
@@ -120,7 +120,6 @@ class AbstractIconTest extends TestCase {
 	protected function invokeMethod(object $object, string $methodName, array $parameters = []): mixed {
 		$reflection = new \ReflectionClass(get_class($object));
 		$method = $reflection->getMethod($methodName);
-		$method->setAccessible(true);
 
 		return $method->invokeArgs($object, $parameters);
 	}
